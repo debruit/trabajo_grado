262ab4d138bd8d9e512d8f1aae3adf3c945927f0
debruit <72114446+debruit@users.noreply.github.com>
2022-09-09 16:18:20 -0500

    Imagenes

--- code/impl_nnunet.py
+++ code/impl_nnunet.py
@@ -1,7 +1,7 @@
-import tensorflow as tf
-import torch
-
-print("Torch: ",torch.cuda.is_available())
-
-      
-print("TF: ",tf.test.is_built_with_cuda())
+import matplotlib.pyplot as plt
+from nilearn import plotting
+plotting.plot_img('/media/takina/DATA/tesis_sebas/nnUNet_raw_data_base/nnUNet_raw_data/Task005_Prostate/imagesTs/prostate_30_0000.nii.gz')
+# plotting.plot_img('/media/takina/DATA/tesis_sebas/nnUNet_raw_data_base/nnUNet_raw_data/Task002_Heart/imagesTs/la_001_0000.nii.gz')
+# plotting.plot_img('/home/takina/OUTPUT_DIRECTORY_HEART/la_001.nii.gz')
+plotting.plot_img('/home/takina/OUTPUT_DIRECTORY/prostate_30.nii.gz')
+plotting.show()
